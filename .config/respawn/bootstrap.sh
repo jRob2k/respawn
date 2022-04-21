d68cfe7742040679957312dfc1a3e0be5061c501
Jay Robinson <jRob2k@gmail.com>
2022-04-21 00:57:54 -0400

    removed 1password crap and replaced with guidance on using a personal access token

--- .config/respawn/bootstrap.sh
+++ .config/respawn/bootstrap.sh
@@ -6,7 +6,6 @@
 
 BOOTSTRAP_INDICATOR=$HOME/.github/.respawn_bootstrap
 RESPAWN_DIRECTORY=$HOME/.config/respawn
-RESPAWN_KEY=$HOME/.ssh/respawn_key
 
 # Script Functions
 # ~~~
@@ -43,29 +42,12 @@ install_thing () {
             # Salt Installation
             curl -o bootstrap-salt.sh -L https://bootstrap.saltproject.io && sudo sh bootstrap-salt.sh
             ;;
-        "1Password")
-            if [[ $OSTYPE = linux-gnu* ]]; then
-                #Adding the key for the 1Password Apt repository
-                curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-                sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-
-                #Add the 1Password Apt repository
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-                sudo tee /etc/apt/sources.list.d/1password.list
-
-                # Add the debsig-verify policy:
-                sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-                curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-                sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-                sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-                curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-                sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-
-                #Install 1Password CLI
-                sudo apt update && sudo apt install 1password-cli
-            fi
+        "Git")
+            # Git Installation
+            echo "Cross platform git installation not implemented. For now, just install using appropriate means and try again."
+            exit 0
             ;;
-        *)
+       *)
             # Didn't follow instructions...
             echo "No software installation script configured for $1"
             ;;
@@ -85,45 +67,19 @@ echo "~~~~"
 
 # Checking for git file. Skipping if there isn't one
 if [[ ! -d ~/.git ]]; then
-    # Check for respawn key and get one if it doesn't
-    if [[ -e $RESPAWN_KEY ]]; then
-        echo "Respawn key, already exists!"
-    else
-        echo "No Respawn key, obtaining one...."
-        # Check for and install 1Password
-        echo "$(check_for_software "1Password" "op")"
-        # Sign into 1Password, get my respawn key and save to .ssh.
-         # Create ssh directory if it doesn't exist
-        if [[ ! -d "$HOME/.ssh" ]]; then
-            mkdir $HOME/.ssh
-        fi
-        touch $RESPAWN_KEY
-        echo "Signing into 1Password"
-        if [[ -z $OP_SESSION_respawn ]]; then
-            eval $(op signin)
-        fi
-        # Getting respawn ssh key
-        ssh_key=$(op item get "respawn - SSH Key" --fields label=notesPlain)
-        # Strip quotes from the key (artifact of 1password) and save as respawn key
-        echo "${ssh_key:1:-1}" > $RESPAWN_KEY
-        sudo chmod 0600 $RESPAWN_KEY
-
-        #Sign out!
-        op signout
-        unset OP_SESSION_respawn
-    fi        
-    # Git config files
-    # Setting the "GIT_SSH_COMMAND" env variable so git uses the respawn key
-    export GIT_SSH_COMMAND="ssh -i $RESPAWN_KEY -o IdentitiesOnly=yes"
+     # Create ssh directory if it doesn't exist
+    if [[ ! -d "$HOME/.ssh" ]]; then
+        mkdir $HOME/.ssh
+    fi
+      # Git config files
     echo "'Git-ing' my config files... hehe..."
     cd ~/
     git config --global init.defaultBranch main
     git init
-    git remote add origin git@github.com:jRob2k/respawn
+    git remote add origin https://github.com/jRob2k/respawn.git
+    echo "Using https to fetch config files. Snag a Personal Access token and login with that"
     git fetch
     git checkout -f main
-    # Unsetting GIT_SSH_COMMAND so normal SSH keys from the config are used
-    unset GIT_SSH_COMMAND
     echo "Got my config files!!!"
 else
     echo "The home directory already has a git file. Skipping git steps."
